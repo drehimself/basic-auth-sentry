a019254740f933503443ba2943e00a5a08d1fb21
Andre Madarang <andre_madarang@hotmail.com>
2014-05-09 15:38:32 -0400

    cleaned up basic auth

--- app/config/database.php
+++ app/config/database.php
@@ -55,7 +55,7 @@ return array(
 		'mysql' => array(
 			'driver'    => 'mysql',
 			'host'      => 'localhost',
-			'database'  => 'foto',
+			'database'  => 'basicauth',
 			'username'  => 'root',
 			'password'  => 'root',
 			'charset'   => 'utf8',
